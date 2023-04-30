a5f66954043905ed107e2cd500e729a4fb183271
Juan Pablo Pinzon <juanpablopinzon3717@gmail.com>
2023-04-29 22:11:23 -0500

    adding functions

--- functions.js
+++ functions.js
@@ -1,9 +1,7 @@
 var imagen = document.querySelector(".image");
-
-
+var showtext = document.querySelector(".text");
 
 function showandhideText() {
-    var showtext = document.querySelector(".text");
     if (showtext.style.display === "none") {
         showtext.style.display = "block"
     } else {
@@ -12,3 +10,16 @@ function showandhideText() {
 }
 
 imagen.addEventListener("click", showandhideText);
+
+var imagen2 = document.querySelector(".image2");
+var showtext2 = document.querySelector(".text2");
+
+function showandhideText2() {
+    if (showtext2.style.display === "none") {
+        showtext2.style.display = "block"
+    } else {
+        showtext2.style.display = "none";
+    }
+}
+
+imagen2.addEventListener("click", showandhideText2);
